1763ab8afcbd40c87d4ff7ade73e59d663086456
Alex Artamonov <aartamonov@bsd220.org>
2026-03-30 16:40:44 -0500

    Created paddle object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -5,6 +5,7 @@
 //  Created by Alex Artamonov on 3/30/26.
 //
 var ball = SKShapeNode()
+var paddle = SKSpriteNode()
 
 import SpriteKit
 import GameplayKit
@@ -18,6 +19,7 @@ class GameScene: SKScene {
     func resetGame() {
         // this stuff happens before each game starts
         makeBall()
+        makePaddle()
     }
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
@@ -82,5 +84,25 @@ class GameScene: SKScene {
         ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
         
         addChild(ball) // add ball object to the view
+    }
+    func makePaddle() {
+        paddle.removeFromParent() // remove the paddle, if it exists
+        
+        paddle = SKSpriteNode(
+            color: .white,
+            size: CGSize(width: frame.width / 4, height: 20)
+        )
+        
+        paddle.position = CGPoint(
+            x: frame.midX,
+            y: frame.minY + 125
+        )
+        
+        paddle.name = "paddle"
+        
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+        paddle.physicsBody?.isDynamic = false
+        
+        addChild(paddle)
     }
 }
